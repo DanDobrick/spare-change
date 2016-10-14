9536107d615b9bc95533335051585f9ffa581621
Dan Dobrick & jakefishbain <pair+dandobrick.jakefishbain@devbootcamp.com>
2016-10-14 15:54:03 -0500

    Take notes on STRIPE and PLAID; comment out code not used for testing

--- app/controllers/plaid_controller.rb
+++ app/controllers/plaid_controller.rb
@@ -1,13 +1,20 @@
 class PlaidController < ApplicationController
 
   def create
-    public_token = params[:public_token]
-    metadata = params[:metadata]
-    account_id = metadata['account_id']
-    # binding.pry
+    # public_token = params[:public_token]
+    # metadata = params[:metadata]
+    # account_id = metadata['account_id']
     # exchange_token_response = Plaid::User.exchange_token(public_token, account_id)
-    # @user = Plaid::User.load(:auth, exchange_token_response.access_token)
+    # stripe_bank_token = exchange_token_response.stripe_bank_account_token
+    # access_token = exchange_token_response.access_token
+    # # current_user.plaid_id = access_token
+    # @user = Plaid::User.load(:auth, access_token)
     # @user.auth
-  end
+    old_user = User.load(:connect, access_token)
 
+    binding.pry
+  end
+# to check user transactions AFTER THEYVE ALREADY SIGNED UP
+# old_user.transactions
+# @user.upgrade(:connect) allows us to get transactions
 end
