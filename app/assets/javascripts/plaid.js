57c33d716f9588bef774064e776268a0df09f955
Dan Dobrick <dandobrick@gmail.com>
2016-10-14 11:28:35 -0500

    Initial plaid setup

--- app/assets/javascripts/plaid.js
+++ app/assets/javascripts/plaid.js
@@ -1,14 +1,17 @@
-
 var linkHandler = Plaid.create({
+  selectAccount: true,
   env: 'tartan',
   clientName: 'spareCHANGE',
   // key: '76589eeee668a81308beca8d3499d0',
   key: 'test_key',
   product: 'auth',
   longtail: true,
+  onLoad: function(){
+    // The Link module finished loading.
+  },
   onSuccess: function(public_token, metadata) {
     $.ajax({
-      url: '/users',
+      url: '/INSERT URL HERE',
       method: 'POST',
       data: {public_token: public_token, metadata: metadata}
     }).done(function(response){
