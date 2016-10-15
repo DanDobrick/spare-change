42545a1973f5da636c945730d7b1ffec901358a7
jakefishbain <jakefishbain@gmail.com>
2016-10-15 12:54:13 -0500

    functional search

--- app/controllers/charities_controller.rb
+++ app/controllers/charities_controller.rb
@@ -5,28 +5,15 @@ require 'uri'
 class CharitiesController < ApplicationController
 
   def index
-
-  end
-
-  def search
-    p "hi"
     if params[:search]
      p category = params[:search]
-      url = "http://data.orghunter.com/v1/charitysearch?user_key=#{CHARITY_TOKEN}&searchTerm=#{category}"
-      uri = URI(url)
-      p "helooooo"
-      p response = Net::HTTP.get(uri)
-      @charities = JSON.parse(response)
-
+      url = "data.orghunter.com/v1/charitysearch?user_key=#{ENV['CHARITY_TOKEN']}&searchTerm=#{category}"
+      p url
+      encoded_url = URI.encode(url)
+      uri = URI.parse(encoded_url)
+      response = HTTParty.get('http://'"#{encoded_url}")
+      @charities = response['data']
     end
-
-
-    #@charities =  params[:keyword]).first
-    # if hashtag
-    #   render json: hashtag.tweets.ordered_json
-    # else
-    #   render :nothing => true, status: 404
-    # end
   end
 
 end
