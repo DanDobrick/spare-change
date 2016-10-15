1e65bdde9195e30bef685e931520da722a245100
Dan Dobrick <dandobrick@gmail.com>
2016-10-14 19:17:14 -0500

    fix password validations in user model

--- app/controllers/plaid_controller.rb
+++ app/controllers/plaid_controller.rb
@@ -14,5 +14,8 @@ class PlaidController < ApplicationController
     current_user.plaid_id = access_token
     current_user.stripe_account = stripe_user.id
     current_user.save
+    binding.pry
+
+    redirect_to root_path
   end
 end
