f1cc29a53428b2ebfe7fe50bb908e9a40803a743
jakefishbain <jakefishbain@gmail.com>
2016-10-15 15:07:44 -0500

    add home page, link, and route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def index
+    render '/index'
+  end
 
 
 end
