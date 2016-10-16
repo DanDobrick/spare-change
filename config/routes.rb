ec8150f5083d6fd4e386aedfadd0fc89136e09b7
Audrey Kang <hkang10@gmail.com>
2016-10-15 21:14:58 -0500

    add user edit info and link

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root "application#index"
-  resources :users, only: [:new, :create, :show] do
+  resources :users, only: [:new, :create, :show, :edit, :update] do
     collection do
       post "plaid" => "plaid#create"
     end
