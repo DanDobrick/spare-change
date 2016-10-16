acb5b6072cdfe95937a8504ff044d57d52f507bf
Dan Dobrick <dandobrick@gmail.com>
2016-10-16 12:44:23 -0500

    Refactor top five sorting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,18 +6,24 @@ class ApplicationController < ActionController::Base
 
   def index
     this_month_donations = Donation.where('updated_at > ?', Date.today.at_beginning_of_month)
-    top_five_array =  Donation.group(:current_charity_ein).sum(:user_bucket).first(5)
-    eins = top_five_hash.map{|ein_sum_pair| ein_sum_pair[0]}
-    @top_five = []
-    top_five_array.each do |top|
-      url = "data.orghunter.com/v1/charitysearch?user_key=#{ENV['CHARITY_TOKEN']}&ein=#{top[0]}"
-      encoded_url = URI.encode(url)
-      uri = URI.parse(encoded_url)
-      response = HTTParty.get('http://'"#{encoded_url}")
-      charity_name = response['data'][0]['charityName']
-      @top_five.push(charity_name)
+    donation_sum_array =  Donation.group(:current_charity_ein).sum(:user_bucket)
+    top_five_array = donation_sum_array.sort_by{|array| array[1]}.reverse[0..4]
+
+    @top_five = top_five_array.map do |ein, amount|
+      charity_name = get_charity_name(ein)
+      {name: charity_name, amount: amount}
     end
     render '/index'
+  end
+
+  private
+
+  def get_charity_name(ein)
+    url = "data.orghunter.com/v1/charitysearch?user_key=#{ENV['CHARITY_TOKEN']}&ein=#{ein}"
+    encoded_url = URI.encode(url)
+    uri = URI.parse(encoded_url)
+    response = HTTParty.get("http://#{encoded_url}")
+    response['data'][0]['charityName']
   end
 
 
