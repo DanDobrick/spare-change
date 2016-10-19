b5f0a456b510e7a8815674b66089c58850acffed
Dan Dobrick <dandobrick@gmail.com>
2016-10-19 16:22:09 -0500

    adjust search terms

--- app/assets/javascripts/searchTypeAhead.js
+++ app/assets/javascripts/searchTypeAhead.js
@@ -39,8 +39,8 @@ $(document).on('ready', function(){
     async: 'true',
     source: charities,
     limit: 10,
-    templates:{ notFound: function(query){return '<div class= query-show>' + query.query + ' Not Found</div>'},
-              pending: function(query){return '<div class= query-show>Searching for: ' + query.query +'</div>'},
+    templates:{ notFound: function(query){return '<div class= query-show>"' + query.query + '" does not match any charity name</div>'},
+              pending: function(query){return '<div class= query-show>Searching for: "' + query.query +'"</div>'},
               suggestion: function(query){return suggestionHTML(query)}
             }
   }).bind('typeahead:select', function(ev, suggestion) {
